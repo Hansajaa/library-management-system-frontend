25428202142cdb0969f030d8514b5b7c29eebea9
Hansajaa <nilanahansaja21@gmail.com>
2024-02-06 23:47:03 +0530

    book update

--- src/app/pages/view-all-books/view-all-books.component.ts
+++ src/app/pages/view-all-books/view-all-books.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component,OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-all-books',
@@ -38,7 +39,26 @@ export class ViewAllBooksComponent implements OnInit{
       this.http.delete(api,{responseType :`text`}).subscribe((response:String)=>{
         console.log(response);
         this.loadBooks();
+        Swal.fire({
+          title: "Deleted !",
+          text: `${this.selectedBook.title} is deleted `,
+          icon: "success"
+        });
         this.selectedBook=null;
+      })
+    }
+
+    saveBook(){
+      let postApi="http://localhost:8080/book/add";
+      this.http.post(postApi,this.selectedBook).subscribe(data=>{
+        console.log(data);
+        this.loadBooks();
+        Swal.fire({
+          title: "Updated !",
+          text: `${this.selectedBook.title} is Updated `,
+          icon: "success"
+        });
+        this.selectedBook={};
       })
     }
 
