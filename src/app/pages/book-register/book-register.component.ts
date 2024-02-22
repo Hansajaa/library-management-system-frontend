b5b5d55d476d0130d1ea1e99690040ce4d9f684f
Hansajaa <nilanahansaja21@gmail.com>
2024-02-22 18:58:16 +0530

    Book Registration successfully integrated with backend

--- src/app/pages/book-register/book-register.component.ts
+++ src/app/pages/book-register/book-register.component.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit, DoCheck, ChangeDetectionStrategy, OnChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-book-register',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, FormsModule,CommonModule,RouterLink],
   templateUrl: './book-register.component.html',
-  styleUrl: './book-register.component.css'
+  styleUrl: './book-register.component.css',
 })
-export class BookRegisterComponent {
+export class BookRegisterComponent{
 
+  private http:HttpClient;
+
+  constructor(private httpClient:HttpClient){
+    this.http=httpClient;
+  }
+
+  book = 
+  {
+    isbn:null,
+    title:null,
+    author:null,
+    category:null,
+    qty:null,
+  };
+  
+  isSaveButtonEnable:boolean=true;
+
+  saveBook(){
+    if(this.book.isbn !== null && this.book.title !== null && this.book.author !== null && this.book.category !== null && this.book.qty !== null){
+      
+      this.isSaveButtonEnable=false;
+      let postApi="http://localhost:8080/book/add";
+      this.http.post(postApi,this.book).subscribe(()=>{
+          this.successfullAlert();
+          this.book.isbn=null;
+          this.book.title=null;
+          this.book.author=null;
+          this.book.category=null;
+          this.book.qty=null;
+          this.isSaveButtonEnable=true;
+      })
+    
+    }else{
+
+      this.warningAlert();
+    
+    }
+  }
+
+  successfullAlert(){
+    Swal.fire({
+      title: "Saved!",
+      text: `${this.book.title} is saved`,
+      icon: "success"
+    });
+  }
+  
+  warningAlert(){
+    Swal.fire({
+      title: "Enter All Details!",
+      text: "You're not complete the form!",
+      icon: "warning"
+    });
+  }
 }
